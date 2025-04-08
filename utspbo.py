9b12fb3a074f3b5d9264b622e91f97003d27ebe0
indira0404 <sintamala1220@gmail.com>
2025-04-08 13:33:12 +0700

    connect db

---
+++ utspbo.py
@@ -0,0 +1,14 @@
+import mysql.connector
+from datetime import datetime
+
+def connect_db():
+    try:
+        return mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="zakat_db"
+        )
+    except mysql.connector.Error as e:
+        print(f"Error: Gagal koneksi ke database - {str(e)}")
+        exit(1)
